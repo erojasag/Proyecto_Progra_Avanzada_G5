4136a3a3a6413c732bf5ba9b1ccabffe359f418e
Emanuel Rojas <eroaguero01@gmail.com>
2022-08-22 17:27:56 -0600

    anadido del ojo para visualizar el password

---
+++ Aplicacion/Aplicacion/Funciones/Password.js
@@ -0,0 +1,21 @@
+﻿
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#inputPassword');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+/*function TogglePass() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}*/
